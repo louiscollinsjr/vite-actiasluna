efa382fc800ea87e14058ef9e651ee19f3dd2aa6
louiscollinsjr <louiscollinsjr@gmail.com>
2024-08-27 16:20:11 +0300

    updated heo component

--- src/components/Route.Home.Component.tsx
+++ src/components/Route.Home.Component.tsx
@@ -3,12 +3,9 @@ import { Header127 } from './Hero.Component';
 
 const HomeComponent: React.FC = () => {
   return (
-    <div className="my-component">
-    <h1 className="text-2xl font-bold">HomeComponent</h1>
-    <p className="text-gray-700">
-      This is a basic React component built with Vite and TypeScript.
-    </p>
-  </div>
+    <>
+    <Header127 />
+    </>
   );
 };
 
